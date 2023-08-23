64522211a66d546e592c39abe19158c5e26763b5
Karoly <karoly.webdev@gmail.com>
2023-08-23 21:05:43 +0100

    Fixing checkWin function error, deleting other branches

--- src/App.js
+++ src/App.js
@@ -1,238 +1,106 @@
 import React, { useState } from 'react';
 import './css/App.css'
 
-const GRID_WIDTH = 10;
-const GRID_HEIGHT = 10;
-const P1_COLOR = 'yellow';
-const P2_COLOR = 'red';
+const GRID_WIDTH = 8;
+const GRID_HEIGHT = 8;
+const P1_COLOR = 'red';
+const P2_COLOR = 'blue';
 const WIN_LIMIT = 3;
 
 function App() {
-  let rows = new Array(GRID_HEIGHT).fill(null);
-  let cols = new Array(GRID_WIDTH).fill(null);
 
-  const [player, setPlayer] = useState(1);
-  const [names, setNames] = useState({ nameOne: 'Karoly', nameTwo: 'Joanna' });
-  const [scores, setScores] = useState({ playerOne: 0, playerTwo: 0 });
+  const [activePlayer, setActivePlayer] = useState(1);
 
-  const [grid, setGrid] = useState(Array.from({ length: GRID_HEIGHT }, () => new Array(GRID_WIDTH).fill(null)));
+  // Step-1, generate table
+  // Step-2: Generate board
+  // Step-3: Click handling
+  // Step-4: Check if there is winner
 
+  // #1
+  const [table, setTable] = useState(new Array(GRID_HEIGHT).fill().map(() => new Array(GRID_WIDTH).fill(0)));
 
-  function winnerCheckHorizontal(coordinateX, coordinateY, playerNumber, gridWidth = GRID_WIDTH, winLimit = WIN_LIMIT) {
-    let count = 0;
-    let y = coordinateY;
-    // Right check
-    for (let x = coordinateX; x < gridWidth; x++) {
-      if (grid[y][x] === playerNumber) {
-        count++;
-      } else {
-        break;
-      }
-    }
-    if (count >= winLimit) {
-      console.log(`Player-${playerNumber} won!`);
-      let newScores = { ...scores };
-      if (playerNumber === 1) {
-        newScores.playerOne++;
-      }
-      if (playerNumber === 2) {
-        newScores.playerTwo++;
-      }
-      setScores(oldScores => oldScores = { ...newScores });
-      //console.log(newScores);
-      return;
-    }
+  //console.table(table);
 
-    // Left check
-    for (let x = coordinateX - 1; x >= 0; x--) {
-      if (grid[y][x] === playerNumber) {
-        count++;
-      } else {
-        break;
-      }
-    }
-    if (count >= winLimit) {
-      console.log(`Player-${playerNumber} won!`);
-      let newScores = { ...scores };
-      if (playerNumber === 1) {
-        newScores.playerOne++;
-      }
-      if (playerNumber === 2) {
-        newScores.playerTwo++;
-      }
-      setScores(oldScores => oldScores = { ...newScores });
-      //console.log(newScores);
-      return;
-    }
-  }
+  // #4
+  const checkWin = (coordinateX, coordinateY, player) => {
+    let countHorizontal = 0;
+    let countVertical = 0;
 
-  function winnerCheckVertical(coordinateX, coordinateY, playerNumber, gridWidth = GRID_WIDTH, winLimit = WIN_LIMIT) {
-    let count = 0;
-    let x = coordinateX;
-    // Up check
-    for (let y = coordinateY; y >= 0; y--) {
-      if (grid[y][x] === playerNumber) {
-        count++;
-      } else {
-        break;
-      }
-    }
-    if (count >= winLimit) {
-      console.log(`Player-${playerNumber} won!`);
-      return;
-    }
-
-    // Down check
-    for (let y = coordinateY + 1; y < GRID_HEIGHT; y++) {
-      if (grid[y][x] === playerNumber) {
-        count++;
-      } else {
-        break;
-      }
-    }
-    if (count >= winLimit) {
-      console.log(`Player-${playerNumber} won!`);
-      return;
-    }
-  }
-
-  function winnerCheckDiagonal(coordinateX, coordinateY, playerNumber, gridWidth = GRID_WIDTH, winLimit = WIN_LIMIT) {
-    let count = 0;
     let x = coordinateX;
     let y = coordinateY;
-    // Up-Right
-    while (x < GRID_WIDTH && y >= 0) {
-      if (grid[y][x] === playerNumber) {
-        count++;
-      } else {
-        break;
-      }
-      if (count >= winLimit) {
-        console.log(`Player-${playerNumber} won!`);
-        return;
-      }
+
+    // Horizontal check
+    while (table[y][x] === player) {
       x++;
-      y--;
+      countHorizontal++;
     }
-    // Down-Left
+
     x = coordinateX - 1;
-    y = coordinateY + 1;
-    while (x >= 0 && y < GRID_HEIGHT) {
-      if (grid[y][x] === playerNumber) {
-        count++;
-      } else {
-        break;
-      }
-      if (count >= winLimit) {
-        console.log(`Player-${playerNumber} won!`);
-        return;
-      }
+    while (table[y][x] === player) {
       x--;
-      y++;
+      countHorizontal++;
     }
-    // Up-Left
-    x = coordinateX - 1;
+
+    // Vertical check
+    y = coordinateY;
+    while (table[y] && table[y][coordinateX] === player) {
+      y++;
+      countVertical++;
+    }
+
     y = coordinateY - 1;
-    while (x >= 0 && y >= 0) {
-      if (grid[y][x] === playerNumber) {
-        count++;
-      } else {
-        break;
-      }
-      if (count >= winLimit) {
-        console.log(`Player-${playerNumber} won!`);
-        return;
-      }
-      x--;
+    while (table[y] && table[y][coordinateX] === player) {
       y--;
-    }
-    // Down-Right
-    x = coordinateX + 1;
-    y = coordinateY + 1;
-    while (x < GRID_WIDTH && y < GRID_HEIGHT) {
-      if (grid[y][x] === playerNumber) {
-        count++;
-      } else {
-        break;
-      }
-      if (count >= winLimit) {
-        console.log(`Player-${playerNumber} won!`);
-        return;
-      }
-      x++;
-      y++;
+      countVertical++;
     }
 
-  }
+    console.log('----------------', 'Horizontal:', countHorizontal, 'Vertical:', countVertical);
+  };
 
+
+  // #3
   const handleClick = (e) => {
-    const x = parseInt(e.target.getAttribute('data-x'));
-    const y = parseInt(e.target.getAttribute('data-y'));
+    let childId = e.target.getAttribute('data-child-id');
+    let child = document.getElementById(`${childId}`);
 
-    if (grid[y][x] === null) {
-      const newGrid = [...grid];
-      newGrid[y][x] = player;
-      setGrid(newGrid);
-    }
-
-    //console.table(grid);
-
-    winnerCheckHorizontal(x, y, 1);
-    winnerCheckHorizontal(x, y, 2);
-
-    winnerCheckVertical(x, y, 1);
-    winnerCheckVertical(x, y, 2);
-
-    winnerCheckDiagonal(x, y, 1);
-    winnerCheckDiagonal(x, y, 2);
-
-
-    let isUsed = e.target.getAttribute('data-used-by');
-
-    if (!isUsed) e.target.setAttribute('data-used-by', player === 1 ? 2 : 1);
-
-    if (player === 1 && !isUsed) {
-      e.target.style.backgroundColor = P1_COLOR;
-      //console.log(P1_COLOR, `x: ${x}`, `y: ${y}`);
-    } else if (player === 2 && !isUsed) {
-      e.target.style.backgroundColor = P2_COLOR;
-      //console.log(P2_COLOR, `x: ${x}`, `y: ${y}`);
+    // Get position in the table
+    let x = Number(childId[0]);
+    let y = Number(childId[1]);
+    //console.log(x, y);
+    if (table[y][x] !== 0) {
+      console.log('Sorry, cannot chose this!')
     } else {
-      console.log('Sorry, cell is aleady occupied!')
-    }
+      child.style.display = 'flex';
+      child.style.backgroundColor = activePlayer === 1 ? P1_COLOR : P2_COLOR;
+      setActivePlayer(oldPlayer => oldPlayer === 1 ? 2 : 1);
 
-    if (!isUsed) {
-      setPlayer(oldPlayer => oldPlayer === 1 ? 2 : 1)
+      // Clone new table
+      let newTable = [...table];
+      newTable[y][x] = activePlayer === 1 ? 1 : 2;
+      //console.table(newTable);
+      // Set table
+      setTable(oldTable => oldTable = [...newTable]);
+
+      //console.table(newTable);
+
+      checkWin(x, y, activePlayer);
     }
-  }
+  };
 
   return (
     <div className="App">
-      <h1>{player === 1 ? "Karoly's turn" : "Joanna's turn"}</h1>
       <div className='container'>
-        <div className='player-house'>
-          <h2>{names.nameOne}</h2>
-          <h3>Score: {scores.playerOne}</h3>
-        </div>
-        <div className='table'>
-          {rows.map((row, rowIndex) => (
-            <div className="row" key={rowIndex}>
-              {cols.map((col, colIndex) => (
-                <div
-                  className='cell'
-                  data-x={colIndex}
-                  data-y={rowIndex}
-                  data-used-by={undefined}
-                  onClick={handleClick}
-                  key={colIndex}>
+        {/* #2 */}
+        <div className='board'>
+          {table.map((row, rowIndex) => (
+            <div className='row' key={rowIndex}>
+              {row.map((col, colIndex) => (
+                <div className='cell' key={colIndex} data-child-id={`${colIndex}${rowIndex}`} onClick={handleClick}>
+                  <div className='disk' id={`${colIndex}${rowIndex}`}>{colIndex}{rowIndex}</div>
                 </div>
               ))}
             </div>
           ))}
-        </div>
-        <div className='player-house'>
-          <h2>{names.nameTwo}</h2>
-          <h3>Score: {scores.playerTwo}</h3>
         </div>
       </div>
     </div>
