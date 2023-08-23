eba9cbd74e284bd5deeaf86b09a761ef82fb0903
Karoly <karoly.webdev@gmail.com>
2023-08-23 22:59:27 +0100

    Adding startNewGame function

--- src/App.js
+++ src/App.js
@@ -10,11 +10,13 @@ const WIN_LIMIT = 3;
 function App() {
 
   const [activePlayer, setActivePlayer] = useState(1);
+  const [gameFinished, setGameFinished] = useState(false);
 
-  // Step-1, generate table
-  // Step-2: Generate board
-  // Step-3: Click handling
-  // Step-4: Check if there is winner
+  // #1, generate table
+  // #2: Generate board
+  // #3: Click handling
+  // #4: Check if there is winner
+
 
   // #1
   const [table, setTable] = useState(new Array(GRID_HEIGHT).fill().map(() => new Array(GRID_WIDTH).fill(0)));
@@ -90,43 +92,61 @@ function App() {
       countDiagonal2++;
     }
 
-    console.log(`Player-${activePlayer}:`, 'Horizontal:', countHorizontal, 'Vertical:', countVertical, 'Diagonal-1:', countDiagonal1, 'Diagonal-2:', countDiagonal2);
+    if (countHorizontal >= WIN_LIMIT || countVertical >= WIN_LIMIT || countDiagonal1 >= WIN_LIMIT || countDiagonal2 >= WIN_LIMIT) {
+      console.log(`Player-${activePlayer} won!`);
+      setGameFinished(oldValue => oldValue = true);
+    };
+
+    // console.log(`Player-${activePlayer}:`, 'Horizontal:', countHorizontal, 'Vertical:', countVertical, 'Diagonal-1:', countDiagonal1, 'Diagonal-2:', countDiagonal2);
   };
 
+  const startNewGame = () => {
+    const allDisks = document.querySelectorAll('.disk');
+    allDisks.forEach((disk) => {
+      disk.style.display = 'none';
+    });
+    let newTable = new Array(GRID_HEIGHT).fill().map(() => new Array(GRID_WIDTH).fill(0));
+    setTable(oldTable => oldTable = [...newTable]);
+    setActivePlayer(oldPlayer => oldPlayer = 1);
+    setGameFinished(oldValue => oldValue = false);
+  };
 
   // #3
   const handleClick = (e) => {
-    let childId = e.target.getAttribute('data-child-id');
-    let child = document.getElementById(`${childId}`);
+    if (!gameFinished) {
+      let childId = e.target.getAttribute('data-child-id');
+      let child = document.getElementById(`${childId}`);
 
-    // Get position in the table
-    let x = Number(childId[0]);
-    let y = Number(childId[1]);
-    //console.log(x, y);
-    if (table[y][x] !== 0) {
-      console.log('Sorry, cannot chose this!')
+      // Get position in the table
+      let x = Number(childId[0]);
+      let y = Number(childId[1]);
+      //console.log(x, y);
+      if (table[y][x] !== 0) {
+        console.log('Sorry, cannot chose this!')
+      } else {
+        child.style.display = 'flex';
+        child.style.backgroundColor = activePlayer === 1 ? P1_COLOR : P2_COLOR;
+        setActivePlayer(oldPlayer => oldPlayer === 1 ? 2 : 1);
+
+        // Clone new table
+        let newTable = [...table];
+        newTable[y][x] = activePlayer === 1 ? 1 : 2;
+        //console.table(newTable);
+        // Set table
+        setTable(oldTable => oldTable = [...newTable]);
+
+        //console.table(newTable);
+
+        checkWin(x, y, activePlayer);
+      }
     } else {
-      child.style.display = 'flex';
-      child.style.backgroundColor = activePlayer === 1 ? P1_COLOR : P2_COLOR;
-      setActivePlayer(oldPlayer => oldPlayer === 1 ? 2 : 1);
-
-      // Clone new table
-      let newTable = [...table];
-      newTable[y][x] = activePlayer === 1 ? 1 : 2;
-      //console.table(newTable);
-      // Set table
-      setTable(oldTable => oldTable = [...newTable]);
-
-      //console.table(newTable);
-
-      checkWin(x, y, activePlayer);
+      console.log('Game has finished!');
     }
   };
 
   return (
     <div className="App">
       <div className='container'>
-        {/* #2 */}
         <div className='board'>
           {table.map((row, rowIndex) => (
             <div className='row' key={rowIndex}>
@@ -138,6 +158,7 @@ function App() {
             </div>
           ))}
         </div>
+        <button onClick={startNewGame}>New Game</button>
       </div>
     </div>
   );
